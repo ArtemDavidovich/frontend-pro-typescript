172306c0dda426d67f1c755f231f56a3f3798f23
Artem Davidovich <chumazek@gmail.com>
2025-02-07 17:10:27 +0100

    fix

--- src/components/storePage/StorePage.tsx
+++ src/components/storePage/StorePage.tsx
@@ -3,6 +3,8 @@ import { IStore } from "../store/Store";
 import styles from "./storePage.module.css";
 import { Link, useParams } from "react-router-dom";
 import Loader from "../loader/Loader";
+import MyButton from "../myButton/MyButton";
+import { useCart } from "../../context/CartContext";
 
 const initialProduct: IStore = {
   id: 0,
@@ -24,6 +26,8 @@ export default function StorePage(): JSX.Element {
   const [product, setProduct] = useState<IStore>(initialProduct);
 
   const { id } = useParams();
+
+  const {addToCart} = useCart()
 
   useEffect(() => {
     setTimeout(() => {
@@ -55,8 +59,21 @@ export default function StorePage(): JSX.Element {
               )}
               €
             </p>
+            <div style={{display: 'flex', justifyContent: 'center', gap: '15px'}}>
             <p style={{ fontSize: "medium" }}>Stock: {product.stock}</p>
-            <p style={{ fontSize: "medium" }}>Rating: {product.rating}</p>
+            <p style={{ fontSize: "medium" }}>Rating: {product.rating}</p></div>
+            <MyButton
+                    text="Add to cart"
+                    func={() =>
+                      addToCart({
+                        id: product.id,
+                        title: product.title,
+                        image: product.thumbnail,
+                        price: (product.price * (1 - product.discountPercentage / 100)),
+                        quantity: 1
+                      })
+                    }
+                  />
           </div>
           <Link to="/store" className={styles.backLink}>
             Go back to homepage
